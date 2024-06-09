e3e7712c66798ab5f250bdbcfce9abdefc46ca33
Tanbir Ahmmed Sihab <sihab.cse@gmail.com>
2024-06-09 23:48:46 +0600

    max array

--- max.js
+++ max.js
@@ -27,3 +27,21 @@ console.log(max)
 
 const maxxx = Math.max(12,32,33,33,43,67)
 console.log(maxxx);
+
+// array max 
+
+const height = [45,45,54,654,7544,45,9756,566]
+
+function getMaxx(numbers){
+    let max = numbers[0];
+
+    for (const num of numbers){
+        if(num > max){
+            max = num;
+        }
+    }
+    return max;
+}
+
+const maxxxxx = getMaxx(height);
+console.log(maxxxxx)
